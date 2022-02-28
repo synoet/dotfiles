9ba590679292332a09c5e9f37f72bf22b320aa34
synoet <teonys@nyu.edu>
2022-02-27 23:21:19 -0500

    remove old rofi config

--- rofi/.config/rofi/dracula/source/app_menu/launcher.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-rofi -no-lazy-grab -show combi -modi drun -theme /home/nysteo/.config/rofi/source/app_menu/style.rasi
--- rofi/.config/rofi/dracula/source/emoji_menu/emoji_menu.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-rofi -no-lazy-grab -show emoji -modi emoji -theme /home/nysteo/.config/rofi/source/emoji_menu/style.rasi
