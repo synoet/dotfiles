596166b811bc3d8d87dd0b7db00c979de7cff2be
synoet <teonys@nyu.edu>
2022-06-08 17:45:22 -0400

    nvim: keymaps

---
+++ nvim/.config/nvim/lua/synoet/keymap.lua
@@ -0,0 +1,31 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+-- Split Keymaps
+keymap("n", "<C-v>", ":vsplit<CR>", opts)
+keymap("n", "<C-s>", ":split<CR>", opts)
+
+-- Comment Keymaps
+keymap("n", "<C-c>", ":Commentary<CR>", opts)
+
+-- Neogit Keymaps
+keymap("n", "<C-g>", ":Neogit<CR>", opts)
+
+-- Split Navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Telescope Keybindings
+keymap("n", "<C-p>", ":Telescope find_files<CR>", opts)
+keymap("n", "<C-f>", ":Telescope live_grep<CR>", opts)
+keymap("n", "<C-b>", ":Telescope buffers<CR>", opts)
+
+-- Buffer Navigation
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Visual Mode Indentation 
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
