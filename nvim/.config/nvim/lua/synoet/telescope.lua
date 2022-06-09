f22f1e84e55dbb731e8b201e416cdc33e5c542a4
synoet <teonys@nyu.edu>
2022-06-09 13:33:09 -0400

    nvim: update telescope prefix

--- nvim/.config/nvim/lua/synoet/telescope.lua
+++ nvim/.config/nvim/lua/synoet/telescope.lua
@@ -1,6 +1,9 @@
 require('telescope').setup{
   defaults = {
-    -- ...
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+    file_ignore_patterns = {"%.git/", "node_modules", "coverage/", "__pycache__", "%.0"},
   },
   pickers = {
     find_files = {
