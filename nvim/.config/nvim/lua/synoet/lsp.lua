0c29fdc94c591870c37f34d985cca34eac3b1802
synoet <teonys@nyu.edu>
2022-06-09 20:21:57 -0400

    tmux: clean up

--- nvim/.config/nvim/lua/synoet/lsp.lua
+++ nvim/.config/nvim/lua/synoet/lsp.lua
@@ -11,8 +11,6 @@ require("nvim-lsp-installer").setup({
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
   return
