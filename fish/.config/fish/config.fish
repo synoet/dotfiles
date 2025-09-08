a9c6ae5fba57de9eb6d83133ba62f2269da9cf0a
synoet <teonys@nyu.edu>
2025-09-08 10:35:11 -0400

    some cleanup

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,5 @@
+fish_vi_key_bindings
+fish_add_path ~/.cargo/bin
+
+alias ls="eza --long --git --color"
+alias cd="z"
--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -6,7 +6,6 @@ return {
   { import = "astrocommunity.motion.hop-nvim" },
   { import = "astrocommunity.completion.supermaven-nvim" },
   { import = "astrocommunity.motion.before-nvim" },
-  { import = "astrocommunity.editing-support.yanky-nvim" },
   { import = "astrocommunity.git.git-blame-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.lsp.lsp-signature-nvim" },
